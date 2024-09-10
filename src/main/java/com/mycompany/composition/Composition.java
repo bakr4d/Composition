84ed09d8f054ddc303c40a2a7452310d4ad19ec3
Bakr <mbakr9212@gmail.com>
2024-09-10 23:43:08 +0300

    just Simple example

--- src/main/java/com/mycompany/composition/Composition.java
+++ src/main/java/com/mycompany/composition/Composition.java
@@ -11,6 +11,12 @@ package com.mycompany.composition;
 public class Composition {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        Car c1= new Car();
+        c1.setColor("blue");
+        System.out.println("the color for this Car is "+c1.getColor());
+        
+        
     }
-}
+
+    }
