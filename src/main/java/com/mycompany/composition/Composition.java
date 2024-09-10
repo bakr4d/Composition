178f678fa3d84cb6daac42f92729e124429a4eeb
Bakr <mbakr9212@gmail.com>
2024-09-10 18:35:04 +0300

    I Related It to Github

---
+++ src/main/java/com/mycompany/composition/Composition.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.composition;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Composition {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
